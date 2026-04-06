0657f2dba70cdd5e7ddde95b55f310810e750340
Biswarghya Biswas <iambiswarghya@gmail.com>
2026-04-06 22:58:32 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
+
+const GTM_ID = "GTM-MH36GFJL";
 
 export const metadata: Metadata = {
   title: "VOYAH - Coming Soon",
@@ -19,10 +22,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="gtm" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body className={`${inter.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
-      <script src="/voyah/js/hide-footer.js"></script>
-</body>
+        <Script src="/voyah/js/hide-footer.js" strategy="afterInteractive" />
+      </body>
     </html>
   );
 }
